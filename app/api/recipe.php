e8152f3dfd0781e543b7df79b7198772e13ba717
Browny29 <angelo_brouns@hotmail.com>
2016-12-01 00:09:50 +0100

    Function to insert a single recipe and its ingredients function added

--- app/api/recipe.php
+++ app/api/recipe.php
@@ -16,16 +16,69 @@ $app->get('/api/recipe', function() {
     }    
 });
 
-$app->post('/api/recipe', function() {
-    require_once('dbconnect.php');
-    
+//Post data to add a new recipe
+$app->post('/api/recipe', function($request) {
     $name = $request->getParsedBody()['name'];
     $description = $request->getParsedBody()['description'];
     $user_id = $request->getParsedBody()['user_id'];
-    
-    $query = "INSERT INTO `recipe` (`user_id`, `recipe_name`, `recipe_description`) VALUES (?,?,?)";
+    $steps = $request->getParsedBody()['steps'];
+    $ingredients = $request->getParsedBody()['ingredients'];
+
+    $steps = json_decode($steps);
+    $ingredients = json_decode($ingredients);
+
+    InsertRecipe($name, $description, $user_id, $steps, $ingredients);
+});
+
+//Insert a new recipe into the database 
+function InsertRecipe($name, $description, $user_id, $steps, $ingredients) {
+    require_once('dbconnect.php');
+
+    //Insert the recipe information
+    $query = "INSERT INTO `recipe` (`user_id`, `recipe_name`, `recipe_description`) VALUES (?,?,?); ";
     $stmt = $mysqli->prepare($query);    
-    $stmt->bind_param("sss", $user_id, $name, $description, $surname);
+    $stmt->bind_param("sss", $user_id, $name, $description);
 
     $stmt->execute();
-});
+   
+    $recipe_id = $mysqli->insert_id;
+
+    //Insert all the recipe steps
+    $query = "INSERT INTO `recipe_step` (`recipe_id`, `step_number`, `step_description`) VALUES (?,?,?)";
+    $stmt = $mysqli->prepare($query);
+    for($i = 1; $i <= sizeof($steps); $i++){
+        $step = (string)$steps[$i-1];        
+        $stmt->bind_param("iis", $recipe_id, $i, $step);
+                    
+        $stmt->execute();
+    }
+
+    //Insert all the recipe ingredients
+    $query = "INSERT INTO `ingredient` (`name`, `unit`) VALUES (?,?)";
+    $stmtNewIngredient = $mysqli->prepare($query);
+    $query = "INSERT INTO `recipe_ingredient` (`recipe_id`, `ingredient_id`, `amount`) VALUES (?,?,?)";
+    $stmtIngredient = $mysqli->prepare($query);
+    for($i = 0; $i < sizeof($ingredients); $i++){
+        $name = strtolower($ingredients[$i]->name);
+        $unit = strtolower($ingredients[$i]->unit);
+        $amount = (int)$ingredients[$i]->amount;
+
+        $query = "SELECT id FROM `ingredient` WHERE name = '$name' AND unit = '$unit'";
+        $result = $mysqli->query($query);
+        $data = $result->fetch_assoc();
+        print_r($data);
+        $ingredientId = null;
+
+        if(sizeof($data) == 0){       
+            $stmtNewIngredient->bind_param("ss", $name, $unit);      
+            $stmtNewIngredient->execute();
+
+            $ingredientId = $mysqli->insert_id;
+        }
+        else{
+            $ingredientId = $data['id'];    
+        }
+        $stmtIngredient->bind_param("iii", $recipe_id, $ingredientId, $amount);      
+        $stmtIngredient->execute();
+    }
+};
